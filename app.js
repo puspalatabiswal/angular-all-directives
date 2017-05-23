258be18419989269aa717f3652d9d84ea12ed1ba
lalithalnt <lalitha445@gmail.com>
2017-05-23 16:07:56 +0530

    updated files

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var app=angular.module("myApp" , []);
 
 app.controller("myController" ,["$scope",
-                                 "custFactory" ,
+                                 "custFactory" , 
                                   function($scope ,custFactory){
 $scope.message="hiii"
 $scope.result=custFactory
+        $scope.custom = true;
+        $scope.toggleCustom =
+        function() {
+            $scope.custom = $scope.custom === false ? true: false;
+        };
+
 }])
